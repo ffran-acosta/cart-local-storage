23872facaaf6de557d7e163c38329d39a81db5ab
ffran.acosta <francoaugustoacosta@gmail.com>
2023-02-16 02:48:56 -0300

    url api local storage change

--- client/public/scripts/api.post.ls.js
+++ client/public/scripts/api.post.ls.js
@@ -1,6 +1,6 @@
 const sendCart = async (localStorash) => {
     let body = JSON.stringify(localStorash)
-    await fetch(`http://localhost:7010/cart/local-storage`, {
+    await fetch(`https://cart-client-production.up.railway.app//cart/local-storage`, {
         method: 'POST',
         body: body,
         headers: { 'Content-Type': 'application/json' }
