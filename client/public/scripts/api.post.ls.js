79955fdb7bc848e32ee725c00d4ab230bac402f6
ffran.acosta <francoaugustoacosta@gmail.com>
2023-02-16 10:46:29 -0300

    client dot env

--- client/public/scripts/api.post.ls.js
+++ client/public/scripts/api.post.ls.js
@@ -1,6 +1,6 @@
 const sendCart = async (localStorash) => {
     let body = JSON.stringify(localStorash)
-    await fetch(`https://cart-client-production.up.railway.app/cart/local-storage`, {
+    await fetch(`http://localhost:7010/cart/local-storage` || process.env.URL_CLIENT, {
         method: 'POST',
         body: body,
         headers: { 'Content-Type': 'application/json' }
--- client/source/routes/cart.routes.js
+++ client/source/routes/cart.routes.js
@@ -1,9 +1,7 @@
 const {Router} = require('express');
 const router = Router()
 
-
 const controller = require('../controller/cart.controller');
-
 
 router.get('/products', controller.products)
 router.get('/cart/resume', controller.cart)
