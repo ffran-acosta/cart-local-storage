4aef3b0dcbc0ab8e3ab077f63e8fc24aa2995cc5
ffran.acosta <francoaugustoacosta@gmail.com>
2023-02-16 10:58:23 -0300

    urls updates

--- client/public/scripts/api.post.ls.js
+++ client/public/scripts/api.post.ls.js
@@ -1,6 +1,6 @@
 const sendCart = async (localStorash) => {
     let body = JSON.stringify(localStorash)
-    await fetch(`http://localhost:7010/cart/local-storage` || process.env.URL_CLIENT, {
+    await fetch(`http://localhost:7010/cart/local-storage` || process.env.URL_CLIENT_POST, {
         method: 'POST',
         body: body,
         headers: { 'Content-Type': 'application/json' }
--- client/source/controller/cart.controller.js
+++ client/source/controller/cart.controller.js
@@ -7,7 +7,7 @@ module.exports = {
     
     products: async (req, res) => {
         try {
-            const url = await axios.get(`https://cart-local-storage-production.up.railway.app/api/railway/products`)
+            const url = await axios.get(`https://cart-server.up.railway.app/api/railway/products` || process.env.URL_SERVER_PRODUCTS)
             const products = await url.data.data
             productsForCart = await products
             return res.render('index', {products})
