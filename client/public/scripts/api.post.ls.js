7d96aa94069b604ea45d6964dc63e231090ddffe
ffran.acosta <francoaugustoacosta@gmail.com>
2023-02-16 02:50:23 -0300

    url fix

--- client/public/scripts/api.post.ls.js
+++ client/public/scripts/api.post.ls.js
@@ -1,6 +1,6 @@
 const sendCart = async (localStorash) => {
     let body = JSON.stringify(localStorash)
-    await fetch(`https://cart-client-production.up.railway.app//cart/local-storage`, {
+    await fetch(`https://cart-client-production.up.railway.app/cart/local-storage`, {
         method: 'POST',
         body: body,
         headers: { 'Content-Type': 'application/json' }
