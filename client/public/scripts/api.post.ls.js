4bd15db13c31c24e7e7bb29882da7ce4f9f6e5c7
ffran.acosta <francoaugustoacosta@gmail.com>
2023-02-17 06:49:17 -0300

    test

--- client/public/scripts/api.post.ls.js
+++ client/public/scripts/api.post.ls.js
@@ -1,6 +1,6 @@
 const sendCart = async (localStorash) => {
     let body = JSON.stringify(localStorash)
-    await fetch(process.env.URL_CLIENT_POST || `http://localhost:7010/cart/local-storage`, {
+    await fetch(process.env.URL_CLIENT_POST, {
         method: 'POST',
         body: body,
         headers: { 'Content-Type': 'application/json' }
